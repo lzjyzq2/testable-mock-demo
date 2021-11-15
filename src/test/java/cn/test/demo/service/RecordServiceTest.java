ac0f4d349b43872e9c0c68100e8a26db18dc9351
lzjyzq2 <lzjyzq2@hotmail.com>
2021-11-15 11:28:42 +0800

    style:增一点注释

--- src/test/java/cn/test/demo/service/RecordServiceTest.java
+++ src/test/java/cn/test/demo/service/RecordServiceTest.java
@@ -20,11 +20,15 @@ public class RecordServiceTest {
         }
     }
 
+    /**
+     * 测试用例
+     */
     @Test
     public void test_getRecords(){
         RecordService recordService = new RecordService();
         String userid = (String) recordService.getRecords().get(0).get("userid");
         System.out.println(userid);
+        // 期望输出 4396-4396-4396-4396 ， 实际输出 9527-9527-9527-9527
         assertEquals("4396-4396-4396-4396",userid);
     }
 }
